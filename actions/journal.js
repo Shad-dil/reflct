a02838d7638d6db8b09be808f296f9a4007e18de
Dilshad <helloshad3@gmail.com>
2024-12-01 20:21:44 +0530

    Writing Api for Daily Propmts

--- actions/journal.js
+++ actions/journal.js
@@ -1,1 +1,46 @@
 "use server";
+import { MOODS } from "@/app/lib/moods";
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+import { getPixabayImage } from "./public";
+export async function createjournalEntry(data) {
+  try {
+    const { userId } = await auth();
+    if (!userId) throw new Error("unAuthorized");
+
+    //ArcJet Rate Limiting
+
+    const user = await db.user.findUnique({
+      where: {
+        clerkUserId: userId,
+      },
+    });
+    if (!user) throw new Error("User Not Found");
+    const mood = MOODS[data.mood.toUpperCase()];
+    if (!mood) throw new Error("Invalid Mood");
+
+    const moodImageUrl = await getPixabayImage(data.moodQuery);
+    const entry = db.entry.create({
+      data: {
+        title: data.title,
+        content: data.content,
+        mood: mood.id,
+        moodScore: mood.score,
+        moodImageUrl: moodImageUrl,
+        userId: user.id,
+        collectionId: data.collectionId || null,
+      },
+    });
+
+    await db.draft.deleteMany({
+      where: {
+        userId: user.id,
+      },
+    });
+    revalidatePath("/dashboard");
+    return entry;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
--- app/(main)/journal/write/page.jsx
+++ app/(main)/journal/write/page.jsx
@@ -1,5 +1,65 @@
+"use client";
+import { journalSchema } from "@/app/lib/schema";
+import { Input } from "@/components/ui/input";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import dynamic from "next/dynamic";
+import { useForm } from "react-hook-form";
+import "react-quill-new/dist/quill.snow.css";
+import { BarLoader } from "react-spinners";
+const ReactQuill = dynamic(() => import("react-quill-new"), { ssr: false });
+
 const JournalEntryPage = () => {
-  return <div>JournalEntryPage</div>;
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(journalSchema),
+    defaultValues: {
+      title: "",
+      content: "",
+      mood: "",
+      collectionId: "",
+    },
+  });
+
+  const isLoading = false;
+
+  return (
+    <div className="py-8">
+      <form>
+        <h1 className="text-5xl md:text-6xl title-gradient">
+          Whats on your mind?
+        </h1>
+        {isLoading && <BarLoader color="orange" width={"100%"} />}
+        <div className="space-y-2">
+          <label className="text-sm font-md">Title</label>
+          <Input
+            {...register("title")}
+            placeholder="Enter title"
+            className={`py-5 md:text-md ${
+              errors.title ? "border-red-500" : ""
+            }`}
+          />
+        </div>
+        <div className="space-y-2">
+          <label className="text-sm font-md">How are you feeling?</label>
+          <Select>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Theme" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="light">Light</SelectItem>
+              <SelectItem value="dark">Dark</SelectItem>
+              <SelectItem value="system">System</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default JournalEntryPage;
